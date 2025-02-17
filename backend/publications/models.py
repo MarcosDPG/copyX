12ddea3219f475d42be87f9e9985309259796ddc
Euler Ostos <eulerdavidos@gmail.com>
2025-02-17 01:15:51 -0500

    build(backend): add models class to publications

--- backend/publications/models.py
+++ backend/publications/models.py
@@ -1,3 +1,19 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Tweet(models.Model):
+    tweet_id = models.UUIDField(primary_key=True, default= uuid.uuid4, editable=False)
+    content = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Retweet(models.Model):
+    tweet_id = models.UUIDField(primary_key=True, default= uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    tweet_id = models.UUIDField(primary_key=True, default= uuid.uuid4, editable=False)
+    content = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
