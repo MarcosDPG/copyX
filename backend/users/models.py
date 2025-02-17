4535995500737b911db72ca9ee7a3b19ca57ab4b
Euler Ostos <eulerdavidos@gmail.com>
2025-02-17 01:04:47 -0500

    build(backend): create model user class

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,12 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_id = models.UUIDField(primary_key=True, default= uuid.uuid4, editable=False)
+    user_name = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=30, blank= False)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=64)
+    birh_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
