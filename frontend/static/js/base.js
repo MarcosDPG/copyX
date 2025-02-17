f5fa0dc9a7956c12483b9e1bc41b3348438c1716
Marcos Parra <marcos345dav@gmail.com>
2025-02-17 01:01:29 -0500

    perf(frontend): Add menu icons

--- frontend/static/js/base.js
+++ frontend/static/js/base.js
@@ -8,21 +8,65 @@ document.addEventListener("DOMContentLoaded", function () {
                 url = "/home";
             }
 
-            fetch(url, { headers: { "X-Requested-With": "XMLHttpRequest" } })
-                .then(response => response.text())
-                .then(html => {
-                    document.getElementById("content").innerHTML = html;
-                    history.pushState(null, "", url);
-                })
-                .catch(err => console.error("Error cargando la vista:", err));
+            if (isSamePath(location.pathname,url)) {
+                fetch(url, { headers: { "X-Requested-With": "XMLHttpRequest" } })
+                    .then(response => response.text())
+                    .then(html => {
+                        document.getElementById("content").innerHTML = html;
+                        if (url == "/home") {
+                            url = "/";
+                        }
+                        history.pushState(null, "", url);
+                        changeIcon(url);
+                    })
+                    .catch(err => console.error("Error cargando la vista:", err));
+            }
         });
     });
 
     window.addEventListener("popstate", function () {
-        fetch(location.pathname, { headers: { "X-Requested-With": "XMLHttpRequest" } })
+        let p = location.pathname;
+        if (p == "/") {
+            p = "/home";
+        }
+        fetch(p, { headers: { "X-Requested-With": "XMLHttpRequest" } })
             .then(response => response.text())
             .then(html => {
                 document.getElementById("content").innerHTML = html;
+                changeIcon(location.pathname);
             });
     });
+
+    changeIcon(window.location.pathname);
 });
+
+function changeIcon(path) {
+    if (path.endsWith("/") && path.length > 1) {
+        path = path.slice(0, -1);
+    }
+    if (path == "/home" || path == "/home/") {
+        path = "/";
+    }
+    document.querySelectorAll(".icon_menu").forEach(icon => icon.setAttribute("fill", "none"));
+
+    let activeLink = document.querySelector(`.nav-link[href="${path}"]`);
+    if (activeLink) {
+        activeLink.querySelector(".icon_menu").setAttribute("fill", "white");
+    }
+}
+
+function isSamePath(url1, url2) {
+    if (url1.endsWith("/") && url1.length > 1) {
+        url1 = url1.slice(0, -1);
+    }
+    if (url1 == "/home") {
+        url1="/"
+    }
+    if (url2.endsWith("/") && url2.length > 1) {
+        url2 = url2.slice(0, -1);
+    }
+    if (url2 == "/home") {
+        url2="/"
+    }
+    return url1 !== url2;
+}
