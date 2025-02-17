099155698219656f42871d5383d68bef6bdb0af7
Euler Ostos <eulerdavidos@gmail.com>
2025-02-17 01:24:43 -0500

    build(backend): add models for interactions app

--- backend/interactions/models.py
+++ backend/interactions/models.py
@@ -1,3 +1,12 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Like(models.Model):
+    tweet_id = models.UUIDField(primary_key=True, default= uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class TypeLike(models.Model):
+    tweet_id = models.AutoField(primary_key=True, max_digits= 1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
