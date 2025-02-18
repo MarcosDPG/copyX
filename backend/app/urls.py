0cf9ce2fbb00d9fac943c71ddd13a0a00427f2ae
Franklin <franklina.canteb@utadeo.edu.co>
2025-02-18 18:01:21 -0500

    feat(frontend): login y registro
    inclusión vista login y registro

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,12 +1,16 @@
-from django.urls import path, include
-from app.views import base, home, profile, settings_view, settings_partial, search_view
+from django.urls import path
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "index.html") 
+def login(request):
+    return render(request, "login.html") 
+def register(request):
+    return render(request, "register.html") 
+
 urlpatterns = [
-    path("", base),
-    path("home/", home, name="home"),
-    path("profile/", profile, name="profile"),
-    path("settings/", settings_view, name="settings"),
-    path("search/", search_view, name="search"),
-    path("compose/post/", home, name="compose_post"),
-    path('settings/<str:option>/', settings_partial, name='settings_partial'),
-    path("users/", include("users.urls"))
+    path("", home, name = "home"),
+    path("login/", login, name = "login"),
+    path("register/", register, name = "register"),
+
 ]
