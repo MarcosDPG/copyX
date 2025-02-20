0e63a104a2afc45b9039c827ad93215848e98f4f
Marcos Parra <marcos345dav@gmail.com>
2025-02-20 16:04:07 -0500

    feat(backend): Add new path in users: '/'

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.user_operations, name="users"),
     path('auth/singup/', views.register, name='singup'),
     path('auth/login/', views.login_view, name='login'),
 ]
