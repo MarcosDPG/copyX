bb65d1b3a09c17ebd1500718c50ee54e4397dd04
Euler Ostos <eulerdavidos@gmail.com>
2025-02-20 14:50:40 -0500

    refactor(backend): modify user model
    Add the str method to the user model to return the username, define the user email as unique and set de name as not blank.

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,15 +1,18 @@
 import uuid
+
 from django.db import models
 
 class User(models.Model):
     user_id = models.UUIDField(primary_key=True, default= uuid.uuid4, editable=False)
     user_name = models.CharField(max_length=20, unique=True)
-    name = models.CharField(max_length=30, blank= False)
-    email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=64)
+    name = models.CharField(max_length=30, blank=False)
+    email = models.EmailField(max_length=50, unique=True)
+    password = models.CharField(max_length=64, blank=False)
     birh_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "Users"
+    def __str__(self):
+        return f"user_id: {self.user_id} - user_name: {self.user_name} - name: {self.name}"
