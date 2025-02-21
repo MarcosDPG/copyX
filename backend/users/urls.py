6ce69771c4c872d53b8d10e0ef4ca2e688c58d23
ChechoDev1 <sergio.echeverrycla@gmail.com>
2025-02-20 23:03:24 -0500

    feat(full): implement delete account

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import logout_view
+from .views import delete_account
 
 urlpatterns = [
     path('<int:user_id>/', views.user_operations, name="user_id"),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('auth/singup/', views.register, name='singup'),
     path('auth/login/', views.login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('delete-account/', delete_account, name='delete_account'),
 ]
