e3b0799e67ae4a0b4c217ff205f3dc46630c471c
Marcos Parra <marcos345dav@gmail.com>
2025-02-21 22:29:30 -0500

    feat(frontend): Add repost and disrepost funtion

--- frontend/static/js/post.js
+++ frontend/static/js/post.js
@@ -1,5 +1,4 @@
 function likeController(obj) {
-
     if (obj.classList.contains('liked') && obj.getAttribute('id-like')) {
         fetch(`/interactions/likes/${obj.getAttribute('id-like')}`, {
             method: 'DELETE',
@@ -39,6 +38,52 @@ function likeController(obj) {
         .then(data => {
             obj.setAttribute('id-like', data.like_id);
             obj.classList.add('liked');
+            obj.nextElementSibling.textContent = parseInt(obj.nextElementSibling.textContent) + 1;
+        });
+    }
+}
+
+function repostController(obj) {
+    if (obj.classList.contains('reposted') && obj.getAttribute('id-repost')) {
+        fetch(`/retweet/${obj.getAttribute('id-repost')}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': getCookie('csrftoken')
+            },
+            credentials: 'include',
+        })
+        .then(response => {
+            if (response.status === 200) {
+                return response.json();
+            }
+            throw new Error("Error en la solicitud, código: " + response.status);
+        })
+        .then(data => {
+            obj.removeAttribute('id-repost');
+            obj.classList.remove('reposted');
+            obj.nextElementSibling.textContent = parseInt(obj.nextElementSibling.textContent) - 1;
+            loadResources(window.location.pathname);
+        });
+    } else {
+        fetch('/retweet/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': getCookie('csrftoken')
+            },
+            credentials: 'include',
+            body: JSON.stringify({tweet: obj.getAttribute('id-post')})
+        })
+        .then(response => {
+            if (response.status === 201) {
+                return response.json();
+            }
+            throw new Error("Error en la solicitud, código: " + response.status);
+        })
+        .then(data => {
+            obj.setAttribute('id-repost', data.retweet_id);
+            obj.classList.add('reposted');
             obj.nextElementSibling.textContent = parseInt(obj.nextElementSibling.textContent) + 1;
         });
     }
